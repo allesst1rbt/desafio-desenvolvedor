8a04edd5d66fc7fe815385a80c7a1535bc0890a3
carlos dias <carloseduardodc090@gmail.com>
2024-07-27 17:49:30 -0400

    chore: i think is this

--- app/Http/Controllers/Exchange.php
+++ app/Http/Controllers/Exchange.php
@@ -9,9 +9,21 @@ class Exchange extends Controller
 {
     public function index(Request $request)
     {
+        $value = intval($request['value']);
+        $paymentMethodTax = match ($request['paymentMethod']) {
+            'slip' => (1.45/100)* $value,
+            'credit' => (7.63/100)* $value,
+        };
+        $conversionTax = match (true) {
+            $value <=3000=> (3/100)* $value,
+            $value > 3000=>(1/100)* $value,
+        };
+        
+        $value = $value - ($paymentMethodTax + $conversionTax);
         $response = Http::get('https://economia.awesomeapi.com.br/json/last/BRL-USD');
+
         $bid = $response->json()['BRLUSD']['bid'];
-        $value = intval($request['value'])*$bid;
-        return response()->json( $value);
+        $value = intval($value * $bid);
+        return response()->json($value);
     }
 }
